0349cf7758e60a8c1b9d1a2c17aee9cc35b4594e
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-03-22 14:16:10 -0300

    Operador Ternario
    Operador ternario

---
+++ OperadorTernario.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorternario;
+
+/**
+ *
+ * @author 2014200413
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*Operador Ternario
+        int n1;
+        int n2;
+        int resultado;
+        n1 = 4;
+        n2 = 8;
+        resultado = (n1>n2)?0:1;
+        System.out.println(resultado);
+        */
+        /*
+        
+        === OPERADORES RELACIONAIS ===
+        >  |  MAIOR QUE       | 5  > 2 |
+        <  |  MENOR QUE       | 4  < 1 |
+        >= | MAIOR OU IGUAL A | 8 >= 3 |
+        <= | MENOR OU IGUAL A | 6 >= 6 |
+        == | IGUAL A          | 9 == 8 |
+        != | DIFERENTE DE     | 4 != 5 |
+        
+        */
+        
+        
+    }
+    
+}
