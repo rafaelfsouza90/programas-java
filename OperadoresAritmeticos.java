95059edbfd3ec9a75e7071f66bf7ad92e38e5863
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-03-22 13:33:17 -0300

    Operadores Aritméticos
    Curso de Java #07 - Operadores Aritméticos e Classe Math

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author 2014200413
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Calculando a média    
+        int n1 = 3;
+        int n2 = 5;
+        float media = (n1 + n2)/2;
+        System.out.println("A média é igual a: " + media); 
+        */
+        
+        /*Pré-Incremento
+        int numero = 5;
+        int valor = 5 + ++numero;
+        System.out.println(valor);
+       */
+        /*Pós-Incremento
+        int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        System.out.println(numero); 
+        */
+       /*Operadores de Atribuição
+       int x = 4;
+       x += 2; //x = x + 2;
+       System.out.println(x);
+       */
+       /*Arrepondamento
+       float v = 3.8f;
+       int ar = (int) Math.floor(v);
+       System.out.println(ar);
+       // floor - arrendonda para baixo
+       //ceil - arredonda para cima
+       //round - arrendonda
+        */
+       /*Gerador de números
+       double ale = Math.random();
+       System.out.println(ale);
+       //random - gera número aleatório
+       */
+       /*Encontrando o número entre 15 e 50
+       double ale = Math.random();
+       int n = (int) (15 + ale * (50-15));
+       System.out.println(n);
+       */
+       
+    }
+    
+
+       
+}
